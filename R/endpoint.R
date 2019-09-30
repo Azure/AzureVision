f744dbdae7baa26f8035db81246af44277625348
Hong Ooi <hongooi@microsoft.com>
2019-09-30 16:49:02 +1000

    classes now handled by AzureCognitive

--- R/endpoint.R
+++ R/endpoint.R
@@ -1,30 +1,3 @@
-vision_endpoint <- function(url, key=NULL, aad_token=NULL, cognitive_token=NULL)
-{
-    obj <- cognitive_endpoint(url, service_type="ComputerVision", key=key, aad_token=aad_token,
-                              cognitive_token=cognitive_token)
-    class(object) <- c("vision_endpoint", class(obj))
-    object
-}
-
-
-customvision_training_endpoint <- function(url, key=NULL, aad_token=NULL, cognitive_token=NULL)
-{
-    obj <- cognitive_endpoint(url, service_type="CustomVision", key=key, aad_token=aad_token,
-                              cognitive_token=cognitive_token)
-    class(obj) <- c("customvision_training_endpoint", class(obj))
-    obj
-}
-
-
-customvision_prediction_endpoint <- function(url, key=NULL, aad_token=NULL, cognitive_token=NULL)
-{
-    obj <- cognitive_endpoint(url, service_type="CustomVision", key=key, aad_token=aad_token,
-                              cognitive_token=cognitive_token)
-    class(obj) <- c("customvision_prediction_endpoint", class(obj))
-    obj
-}
-
-
 call_cognitive_endpoint.customvision_training_endpoint <- function(endpoint, ...)
 {
     class(endpoint) <- class(endpoint)[-1]
