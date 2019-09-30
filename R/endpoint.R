f97d6230245055adee5939423c106fec734c4484
Hong Ooi <hongooi@microsoft.com>
2019-09-30 16:15:14 +1000

    implement call_endpoint methods

--- R/endpoint.R
+++ R/endpoint.R
@@ -7,12 +7,35 @@ vision_endpoint <- function(url, key=NULL, aad_token=NULL, cognitive_token=NULL)
 }
 
 
-customvision_endpoint <- function(url, key=NULL, aad_token=NULL, cognitive_token=NULL)
+customvision_training_endpoint <- function(url, key=NULL, aad_token=NULL, cognitive_token=NULL)
 {
     obj <- cognitive_endpoint(url, service_type="CustomVision", key=key, aad_token=aad_token,
                               cognitive_token=cognitive_token)
-    class(object) <- c("customvision_endpoint", class(obj))
-    object
+    class(obj) <- c("customvision_training_endpoint", class(obj))
+    obj
+}
+
+
+customvision_prediction_endpoint <- function(url, key=NULL, aad_token=NULL, cognitive_token=NULL)
+{
+    obj <- cognitive_endpoint(url, service_type="CustomVision", key=key, aad_token=aad_token,
+                              cognitive_token=cognitive_token)
+    class(obj) <- c("customvision_prediction_endpoint", class(obj))
+    obj
+}
+
+
+call_cognitive_endpoint.customvision_training_endpoint <- function(endpoint, ...)
+{
+    class(endpoint) <- class(endpoint)[-1]
+    call_cognitive_endpoint(endpoint, ..., auth_header="training-key")
+}
+
+
+call_cognitive_endpoint.customvision_prediction_endpoint <- function(endpoint, ...)
+{
+    class(endpoint) <- class(endpoint)[-1]
+    call_cognitive_endpoint(endpoint, ..., auth_header="prediction-key")
 }
 
 
