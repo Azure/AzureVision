2a50dddbabe096535c5e2ccc37db95bfc042a8b2
Hong Ooi <hongooi@microsoft.com>
2019-10-03 05:21:01 +1000

    more imgs

--- R/customvision_imgs.R
+++ R/customvision_imgs.R
@@ -2,7 +2,7 @@ list_tags <- function(project, iteration=NULL, as=c("names", "dataframe", "list"
 {
     as <- match.arg(as)
     if(as == "list")
-        return(do_training_op(project, "tags"))
+        return(do_training_op(project, "tags", options(iterationId=iteration)))
 
     tags <- do_training_op(project, "tags", simplifyVector=TRUE)
     if(as == "names")
@@ -15,17 +15,15 @@ get_tag <- function(project, name=NULL, id=NULL, iteration=NULL)
 {
     if(is.null(id))
     {
-        tagdf <- list_tags(project, as="dataframe")
-        tag <- tagdf[tagdf$name == name, ]
-        if(nrow(tag) == 0)
+        taglist <- list_tags(project, iteration=iteration, as="list")
+        tagnames <- sapply(taglist, `[[`, "name")
+        i <- which(name == tagnames)
+        if(is_empty(i))
             stop(sprintf("Image tag '%s' not found", name), call.=FALSE)
 
-        tag <- as.list(tag)
-        if(is.na(tag$description))
-            tag["description"] <- list(NULL)
-        tag
+        taglist[[i]]
     }
-    else do_training_op(project, file.path("tags", id))
+    else do_training_op(project, file.path("tags", id), options(iterationid=iteration))
 }
 
 
@@ -81,6 +79,29 @@ add_negative_tag <- function(project, negative_name="_negative_")
     else do_training_op(project, "tags", options=list(name=negative_name, type="Negative"), http_verb="POST")
 
     invisible(project)
+}
+
+
+get_images <- function(project, include=c("tagged", "untagged", "both"), iteration=NULL,
+                       as=c("id", "dataframe", "list"))
+{
+    include <- match.arg(include)
+    as <- match.arg(as)
+    simplify <- as != "list"
+
+    tagged_imgs <- if(include %in% c("tagged", "both"))
+        do_training_op(project, "images/tagged", options=list(iterationId=iteration), simplifyVector=simplify)
+    else NULL
+
+    untagged_imgs <- if(include %in% c("untagged", "both"))
+        do_training_op(project, "images/untagged", options=list(iterationId=iteration), simplifyVector=simplify)
+    else NULL
+
+    if(as == "id")
+        rbind(tagged_imgs, untagged_imgs)$id
+    else if(as == "dataframe")
+        rbind(tagged_imgs, untagged_imgs)
+    else c(tagged_imgs, untagged_imgs)
 }
 
 
