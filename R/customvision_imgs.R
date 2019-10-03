9d761c3dfd789c7078f5e4fec2be3973a8ac4730
Hong Ooi <hongooi@microsoft.com>
2019-10-03 21:23:16 +1000

    tags done

--- R/customvision_imgs.R
+++ R/customvision_imgs.R
@@ -1,4 +1,4 @@
-list_tags <- function(project, iteration=NULL, as=c("names", "dataframe", "list"))
+list_tags <- function(project, iteration=NULL, as=c("names", "ids", "dataframe", "list"))
 {
     as <- match.arg(as)
     if(as == "list")
@@ -7,6 +7,8 @@ list_tags <- function(project, iteration=NULL, as=c("names", "dataframe", "list"
     tags <- do_training_op(project, "tags", options(iterationId=iteration), simplifyVector=TRUE)
     if(as == "names")
         tags$name
+    else if(as == "ids")
+        tags$id
     else as.data.frame(tags)
 }
 
@@ -83,21 +85,21 @@ add_negative_tag <- function(project, negative_name="_negative_")
 
 
 list_images <- function(project, include=c("tagged", "untagged", "both"), iteration=NULL,
-                       as=c("id", "dataframe", "list"))
+                       as=c("ids", "dataframe", "list"))
 {
     include <- match.arg(include)
     as <- match.arg(as)
     simplify <- as != "list"
 
-    tagged_imgs <- if(include %in% c("tagged", "both"))
+    tagged_imgs <- if(include != "untagged")
         do_training_op(project, "images/tagged", options=list(iterationId=iteration), simplifyVector=simplify)
     else NULL
 
-    untagged_imgs <- if(include %in% c("untagged", "both"))
+    untagged_imgs <- if(include != "tagged")
         do_training_op(project, "images/untagged", options=list(iterationId=iteration), simplifyVector=simplify)
     else NULL
 
-    if(as == "id")
+    if(as == "ids")
         rbind(tagged_imgs, untagged_imgs)$id
     else if(as == "dataframe")
         rbind(tagged_imgs, untagged_imgs)
@@ -105,10 +107,53 @@ list_images <- function(project, include=c("tagged", "untagged", "both"), iterat
 }
 
 
-tag_uploaded_images <- function(project, tags, images=list_images(project, "untagged", as="id"))
+tag_uploaded_images <- function(project, tags, images=list_images(project, "untagged", as="ids"))
 {
     tags <- data.frame(imageId=images, tagId=tags, stringsAsFactors=FALSE)
     do_training_op(project, "images/tags", body=list(tags=tags), http_verb="POST")
+    invisible(project)
+}
+
+
+untag_uploaded_images <- function(project, images=list_images(project, "tagged", as="ids"),
+                                  tags=list_tags(project, as="ids"))
+{
+    images <- paste0(images, collapse=",")
+    tags <- paste0(tags, collapse=",")
+    opts <- list(imageIds=images, tagIds=tags)
+
+    do_training_op(project, "images/tags", options=opts, http_verb="DELETE")
+    invisible(project)
+}
+
+
+remove_images <- function(project, images=list_images(project, "untagged", as="ids"), confirm=TRUE)
+{
+    if(!confirm_delete("Are you sure you want to remove images from the project?", confirm))
+        return(invisible(project))
+
+    images <- paste0(images, collapse=",")
+    do_training_op(project, "images", options=list(imageIds=images), http_verb="DELETE")
+    invisible(project)
+}
+
+
+remove_tags <- function(project, tag_names=NULL, tag_ids=NULL, confirm=TRUE)
+{
+    if(!confirm_delete("Are you sure you want to remove tags from the project?", confirm))
+        return(invisible(project))
+
+    if(is.null(tag_names) && is.null(tag_ids))
+        stop("Must provide either tag names or IDs", call.=FALSE)
+    if(!is.null(tag_names) && !is.null(tag_ids))
+        stop("Provide either tag names or IDs, not both", call.=FALSE)
+
+    if(is.null(tag_ids))
+        tag_ids <- get_tag_ids_from_names(tag_names, list_tags(project, as="dataframe"))
+
+    lapply(tag_ids, function(tag)
+        do_training_op(project, file.path("tags", tag), http_verb="DELETE"))
+
     invisible(project)
 }
 
