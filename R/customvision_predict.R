224d124dc6eb14e0753eeeb69471a3b38e7adabc
Hong Ooi <hongooi@microsoft.com>
2019-10-08 10:45:58 +1100

    fix predictions

--- R/customvision_predict.R
+++ R/customvision_predict.R
@@ -2,40 +2,35 @@ predict.customvision_model <- function(object, images, type=c("class", "prob", "
 {
     type <- match.arg(type)
     images <- images_to_bodies(images)
+    files <- !is.null(images[[1]]$content)
+    op <- file.path("quicktest", if(files) "image" else "url")
     opts <- list(iterationId=object$id)
 
-    out <- if(!is.null(images[[1]]$content))
-    {
+    out <- if(files)
         lapply(images, function(f)
-            do_training_op(object$project, "quicktest/image", options=opts, body=f$content,
-                           http_verb="POST",
-                           simplifyVector=TRUE))
-    }
-    else
-    {
-        lapply(images, function(f)
-            do_training_op(object$project, "quicktest/url", options=opts, body=f,
-                           http_verb="POST",
-                           simplifyVector=TRUE))
-    }
+            do_training_op(object$project, op, options=opts, body=f$content, http_verb="POST", simplifyVector=TRUE))
+    else lapply(images, function(f)
+       do_training_op(object$project, op, options=opts, body=f, http_verb="POST", simplifyVector=TRUE))
 
-    lapply(out, `[[`, "predictions")
+    normalize_predictions(out, type)
 }
 
 
-predict.classification_service <- function(object, images, save_result=FALSE, ...)
+predict.classification_service <- function(object, images, type=c("class", "prob", "list"), save_result=FALSE, ...)
 {
-    customvision_predict_internal(object, images, save_result, verb="classify")
+    type <- match.arg(type)
+    customvision_predict_internal(object, images, type, save_result, verb="classify")
 }
 
 
-predict.object_detection_service <- function(object, images, save_result=FALSE, ...)
+predict.object_detection_service <- function(object, images, type=c("class", "prob", "list"), save_result=FALSE, ...)
 {
-    customvision_predict_internal(object, images, save_result, verb="detect")
+    type <- match.arg(type)
+    customvision_predict_internal(object, images, type, save_result, verb="detect")
 }
 
 
-customvision_predict_internal <- function(object, images, save_result, verb)
+customvision_predict_internal <- function(object, images, type, save_result, verb)
 {
     images <- images_to_bodies(images)
     files <- !is.null(images[[1]]$content)
@@ -48,7 +43,8 @@ customvision_predict_internal <- function(object, images, save_result, verb)
             do_prediction_op(object, op, body=f$content, http_verb="POST", simplifyVector=TRUE))
     else lapply(images, function(f)
         do_prediction_op(object, op, body=f, http_verb="POST", simplifyVector=TRUE))
-    out
+
+    normalize_predictions(out, type)
 }
 
 
@@ -82,5 +78,17 @@ object_detection_service <- function(endpoint, project, name)
 
 normalize_predictions <- function(lst, type)
 {
-    labels <- lst[[1]]$name
+    names(lst) <- NULL
+    lst <- lapply(lst, `[[`, "predictions")
+
+    if(type == "list")
+        lst
+    else if(type == "prob")
+    {
+        tagnames <- sort(lst[[1]]$tagName)
+        out <- t(sapply(lst, function(df) df$probability[order(df$tagName)]))
+        colnames(out) <- tagnames
+        out
+    }
+    else sapply(lst, function(df) df$tagName[which.max(df$probability)])
 }
